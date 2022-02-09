b115756df6c2c46af8b130ea19f7876c785abc90
NicolaCortese <nicolacortese92@gmail.com>
2022-02-09 16:20:45 +0000

    turned into a Hash and added a cohort value

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 
 #first we print the list of students
@@ -19,8 +19,8 @@ def print_header
   puts "--------------"
 end
 
-def print_names(students)
-  students.each {|student| puts student}
+def print(students)
+  students.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
 #finally, we print the total number of students
@@ -29,5 +29,5 @@ def print_footer(students)
 end
 
 print_header
-print_names(students)
+print(students)
 print_footer(students)
