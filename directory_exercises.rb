d39c20395386f2a9a1e5082b7fadaa9cd2c36e1a
NicolaCortese <nicolacortese92@gmail.com>
2022-02-10 16:00:17 +0000

    completed exercise 8

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -22,38 +22,38 @@ def print_header
 end
 
 def print(students)
-=begin
-  students.each_with_index do |student,i|
-    if student[:name].length<12
-      puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  end
-=end
+
   i = 0
   while i < students.length
-    puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort, I'm a #{students[i][:hobby]} and I'm from #{students[i][:country]})".center(80, ".")
     i += 1
   end
 
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students!"
 end
 
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name and cohort (November or December) of the students"
   puts "To finish, just hit return twice"
   #create an empty array
   students = []
-  #get the first name
-  name = gets.chomp.capitalize
+  #get the first name and cohort
+  answer = gets.chomp.split(" ").map{|e|e.capitalize}
   #while the name is not empty, repeat this code
-  while !name.empty? do
-    #add the student hash to the array
-    students << {name: name, cohort: :november}
+  while !answer.empty? do
+    #add the student hash to the array with a default value of TBC if he hasn't provided a cohort
+    answer[1] = "TBC" if answer[1] == nil
+    #what if they make a typo
+    while answer[1] != "November" && answer[1] != "December" && answer[1] != "TBC"
+      puts "Please could you repeat the cohort"
+      answer[1] = gets.chomp.capitalize
+    end
+    students << {name: answer[0], cohort: answer[1].to_sym, country: :italy, hobby: :coder}
     puts "Now we have #{students.count} students"
-    name = gets.chomp.capitalize
+    answer = gets.chomp.split(" ").map{|e|e.capitalize}
   end
   #return the array 
   students
